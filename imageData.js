6292d9eb976e8593fb38145319906d3f75a39109
unknown <cam.mac.cm@gmail.com>
2024-10-04 15:31:48 -0600

    Add data for towers images

--- imageData.js
+++ imageData.js
@@ -9,6 +9,110 @@ export const imageData = [
                 storeURL: "",
                 variantSize: "portraitWeb",
                 order: 1
+            },
+            {
+                name: "Vienna_Tower_Blog.JPG",
+                id: "3f38fcd8-30c2-4581-1a22-e441d9f0fb00",
+                caption: "St. Stephen’s Cathedral, Vienna, Austria",
+                storeURL: "",
+                variantSize: "portraitWeb",
+                order: 2
+            },
+            {
+                name: "Hiroshima_Blog.JPG",
+                id: "e350ffba-1aa0-47aa-598b-a5a8ddba7800",
+                caption: "Atomic Bomb Dome, Hiroshima, Japan",
+                storeURL: "",
+                variantSize: "portraitWeb",
+                order: 3
+            },
+            {
+                name: "La_Sagrada_Blog.JPG",
+                id: "179df026-79aa-4364-9b28-0bb20cd75b00",
+                caption: "La Sagrada Familia, Barcelona, Spain",
+                storeURL: "",
+                variantSize: "portraitWeb",
+                order: 4
+            },
+            {
+                name: "Munich_Tower_Blog.JPG",
+                id: "c95dc002-e9fe-43a5-4d01-624b7976bd00",
+                caption: "New Town Hall, Munich, Germany",
+                storeURL: "",
+                variantSize: "portraitWeb",
+                order: 5
+            },
+            {
+                name: "SA_Tower_Blog.JPG",
+                id: "49000271-fd28-4230-78c5-7d89e7ea9100",
+                caption: "Tower of the Americas, San Antonio, Texas, USA",
+                storeURL: "",
+                variantSize: "portraitWeb",
+                order: 6
+            },
+            {
+                name: "Schweppes_Blog.JPG",
+                id: "cb60cef6-32fe-4dc5-877d-0017a5f3ec00",
+                caption: "Edificio Capitol, Madrid, Spain",
+                storeURL: "",
+                variantSize: "portraitWeb",
+                order: 7
+            },
+            {
+                name: "Prague_Tower_Blog.JPG",
+                id: "c770806f-bfa6-496f-e145-21ed3e647600",
+                caption: "Old Town Hall, Prague, Czechia",
+                storeURL: "",
+                variantSize: "portraitWeb",
+                order: 8
+            },
+            {
+                name: "Seville_Tower_Blog.JPG",
+                id: "4c1e4970-1480-40b1-b5f8-d82d4ca61d00",
+                caption: "Seville Cathedral, Seville, Spain",
+                storeURL: "",
+                variantSize: "portraitWeb",
+                order: 9
+            },
+            {
+                name: "Tokyo_Temple_Blog.JPG",
+                id: "740d3193-a7f6-4e67-e489-7204e82cf600",
+                caption: "Senso-ji Temple Pagoda, Tokyo, Japan",
+                storeURL: "",
+                variantSize: "portraitWeb",
+                order: 10
+            },
+            {
+                name: "TX_Capitol_Blog.JPG",
+                id: "217bb791-5739-4e35-5566-e288ef6e8800",
+                caption: "Texas State Capitol, Austin, Texas, USA",
+                storeURL: "",
+                variantSize: "portraitWeb",
+                order: 11
+            },
+            {
+                name: "Prague_Tower2_Blog.JPG",
+                id: "0c2071c5-3a4d-4ebb-2be5-2a648515a800",
+                caption: "Church of Our Lady before Tyn, Prague, Czechia",
+                storeURL: "",
+                variantSize: "portraitWeb",
+                order: 12
+            },
+            {
+                name: "Tokyo_Tower_Blog.JPG",
+                id: "10eb43cd-7c87-42d4-82a8-94755db07400",
+                caption: "NTT Docomo Yoyogi Building, Tokyo, Japan",
+                storeURL: "",
+                variantSize: "portraitWeb",
+                order: 13
+            },
+            {
+                name: "nh-capital2.JPG",
+                id: "d0e16876-7a49-4ad0-66bd-a12294b3da00",
+                caption: "New Hampshire State House, Concord, New Hampshire, USA",
+                storeURL: "",
+                variantSize: "portraitWeb",
+                order: 13
             }
         ]
     },
