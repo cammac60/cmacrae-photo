c74d3d57bc3b76558c306a981f2d94188de87d19
unknown <cam.mac.cm@gmail.com>
2024-10-04 14:30:57 -0600

    Reformat image data and add notes

--- imageData.js
+++ imageData.js
@@ -1,11 +1,29 @@
 export const imageData = [
     {
-        "id": "",
-        "caption": "",
-        "size": "",
-        "category": "",
-        "dimensionHeight": 0,
-        "dimensionWidth": 0,
-        "storeURL": ""
+        category: "Architechture",
+        images: []
+    },
+    {
+        category: "Landscape",
+        images: []
+    },
+    {
+        category: "Street",
+        images: []
+    },
+    {
+        category: "Towers",
+        images: []
     }
 ]
+
+
+
+// Images (arr)
+//
+    // Sort based on positioning in menu
+//
+    // Arch (obj)
+    // Landscape (obj)
+    //Street (obj)
+    //Towers (obj)
