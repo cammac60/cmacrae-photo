69af3b4625ee8dec6fc44e86ff0a71af117290bc
unknown <cam.mac.cm@gmail.com>
2024-10-10 18:11:45 -0600

    Add architecture data

--- imageData.js
+++ imageData.js
@@ -131,9 +131,126 @@ export const imageData = [
         ]
     },
     {
-        category: "Architechture",
+        category: "Architecture",
         images: [
-
+            {
+                name: "CDMX_Temple_Blog.JPG",
+                id: "e49417f6-c570-4fd4-7980-aea4e4009b00",
+                caption: "Pyramid of the Sun, Teotihuacan, State of Mexico, Mexico",
+                storeURL: "",
+                variantSize: "landscapeWeb",
+                order: 1,
+                isBanner: false
+            },
+            {
+                name: "Lisbon_Hotel_Blog.JPG",
+                id: "74179f02-8025-4b72-e56d-9c2edf57d300",
+                caption: "Blue Liberdade Hotel, Lisbon, Portugal",
+                storeURL: "",
+                variantSize: "portraitWeb",
+                order: 2,
+                isBanner: false
+            },
+            {
+                name: "Berlin_Tower3_Blog.JPG",
+                id: "77a1aba4-8406-45a8-f91e-00f09cb74100",
+                caption: "Fernsehtrum, Berlin, Germany",
+                storeURL: "",
+                variantSize: "portraitWeb",
+                order: 3,
+                isBanner: false
+            },
+            {
+                name: "Seoul_Architecture_Blog.JPG",
+                id: "4937bc91-0edf-4ad5-75d9-bb82956cbc00",
+                caption: "Dongdaemun Design Plaza, Seoul, South Korea",
+                storeURL: "",
+                variantSize: "portraitWeb",
+                order: 4,
+                isBanner: true
+            },
+            {
+                name: "Mt_Rushmore_Blog.JPG",
+                id: "ad5d80d9-5542-4447-9d56-42b4bc76f200",
+                caption: "Mt. Rushmore, South Dakota, USA",
+                storeURL: "",
+                variantSize: "portraitWeb",
+                order: 5,
+                isBanner: false
+            },
+            {
+                name: "Alahmbra_Blog.JPG",
+                id: "0cacd99b-753c-4cf8-c14b-5c75df928d00",
+                caption: "Alhambra, Granada, Spain",
+                storeURL: "",
+                variantSize: "landscapeWebWide",
+                order: 6,
+                isBanner: false
+            },
+            {
+                name: "Lisbon_Col_Blog.JPG",
+                id: "41345c01-3647-42cd-afc3-e71509662b00",
+                caption: "Monument to the Discoveries, Lisbon, Portugal",
+                storeURL: "",
+                variantSize: "portraitWeb",
+                order: 7,
+                isBanner: false
+            },
+            {
+                name: "Seoul_apt_Blog.JPG",
+                id: "70e5c613-2e16-467e-4437-3bbd0042bc00",
+                caption: "Seoul, South Korea",
+                storeURL: "",
+                variantSize: "portraitWeb",
+                order: 8,
+                isBanner: false
+            },
+            {
+                name: "Tokyo_Temple2_Blog.JPG",
+                id: "f224679f-d330-473e-cb7c-eaf849e1d100",
+                caption: "Senso-ji Temple Pagoda, Tokyo, Japan",
+                storeURL: "",
+                variantSize: "portraitWeb",
+                order: 9,
+                isBanner: false
+            },
+            {
+                name: "space-needle-22.JPG",
+                id: "4c333569-db1a-419b-6e1e-ba3ad48e2000",
+                caption: "Space Needle, Seattle, Washington, USA",
+                storeURL: "",
+                variantSize: "portraitWeb",
+                order: 10,
+                isBanner: false
+            },
+            {
+                name: "Berlin_Tower1_Blog.JPG",
+                id: "dcc2dc25-136a-4942-f89a-617c431f0c00",
+                caption: "Fernsehtrum, Berlin, Germany",
+                storeURL: "",
+                variantSize: "landscapeWeb",
+                order: 11,
+                isBanner: false
+            },
+            {
+                name: "Tokyo_Tower2_Blog.JPG",
+                id: "ceb1a5f8-b7fe-40fa-8d78-d1b261910600",
+                caption: "Tokyo, Japan",
+                storeURL: "",
+                variantSize: "portraitWeb",
+                order: 12,
+                isBanner: false
+            },
+            {
+                name: "Seoul_Temple_Blog.JPG",
+                id: "e309ef4c-9b81-473f-0ae9-3bd8282a3a00",
+                caption: "Gyeongbokgung Palace, Seoul, South Korea",
+                storeURL: "",
+                variantSize: "portraitWeb",
+                order: 13,
+                isBanner: false
+            }
+        ]
         ]
     },
     {
